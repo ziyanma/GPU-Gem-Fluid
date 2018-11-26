25e403ed36259bea977ff120520641ec35d69b60
Ziyan Ma <ziyanma@usc.edu>
2018-11-26 11:11:13 -0800

    shader setup

--- Assets/Scripts/SmokeSimulation.cs
+++ Assets/Scripts/SmokeSimulation.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmokeSimulation : MonoBehaviour {
+public class SmokeSimulation : AnimationController {
     public Material mat;
+
+    public Texture3D [] mTexture;
+
+    public Vector3Int texRes;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        mTexture = new Texture3D[2];
+        mTexture[0] = new Texture3D(texRes[0], texRes[0], texRes[0], TextureFormat.Alpha8, true); // To Alpha 8
+        mTexture[1] = new Texture3D(texRes[0], texRes[0], texRes[0], TextureFormat.Alpha8, true);
+    }
+
+    void SwapBuffer (Texture3D [] swap)
+    {
+        Texture3D texture = swap[0];
+        swap[0] = swap[1];
+        swap[1] = texture;
+    }
+
+    // Update is called once per frame
+    void Update () {
         // Set up shader
         mat.SetVector("_Scale", transform.localScale);
         mat.SetVector("_Translate", transform.position);
